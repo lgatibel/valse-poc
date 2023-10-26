063dd44cdeaf21313cd8425b44321ccb21c5ae46
Lionel Gatibelza <lionelgatibelza@gmail.com>
2023-10-26 17:36:46 +0200

    add return button to profilCardDetail

--- app/profil/[id]/page.tsx
+++ app/profil/[id]/page.tsx
@@ -1,4 +1,5 @@
 import NotFound from '@/app/components/Error/NotFound'
+import Navigation from '@/app/components/Navigation/Navigation'
 import { getProfil } from '@/app/queries/profilQueries'
 import Image from 'next/image'
 import React from 'react'
@@ -18,10 +19,10 @@ export default async function ProfilDetail({ params }: IProps) {
         <div className="flex flex-col w-full h-screen">
             {profil !== null ?
                 <>
-                    <div>
-                        <Image fill style={{ objectFit: 'cover', width: '100%' }} src={profil.photo} alt="image profil" />
-                        <div className=''></div>
+                    <div className="absolute z-10 px-4 py-8">
+                        <Navigation />
                     </div>
+                    <Image fill style={{ objectFit: 'cover', width: '100%' }} src={profil.photo} alt="image profil" />
                     <div className="absolute bottom-0 h-[30%] left-0 rounded-t-lg bg-primary p-4 flex flex-col gap-8">
                         <div className="flex justify-center">
                             <div className="rounded-lg bg-gray-700 w-12 h-1.5"></div>
@@ -35,7 +36,7 @@ export default async function ProfilDetail({ params }: IProps) {
                         <p>{profil.description}</p>
                     </div>
                 </>
-                : <NotFound/>}
+                : <NotFound />}
 
         </div>
     )

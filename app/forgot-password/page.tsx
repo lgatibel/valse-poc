e53d942bc7db3ee79fcaf58a095405f9767d1a02
Lionel Gatibelza <lionelgatibelza@gmail.com>
2023-11-04 15:40:14 +0100

    fix forgotten page

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -8,17 +8,15 @@ export default function ForgotPassword() {
   return (
     <>
       <BackgoundImage src={bgImage} />
-      <div className="flex flex-col flex-grow p-8 sm:p-4 gap-96">
-        <Navigation action="close"/>
-        <div className="flex flex-col gap-16">
-          <div className="flex flex-col gap-8">
-            <HeroTitle title="Mot de passe oublié ?" />
-            <p>Saisis ton adresse e-mail. Tu recevras un lien pour créer un nouveau mot de passe.</p>
-          </div>
-          <div className="flex flex-col gap-6">
-            <input className="placeholder:text-white bg-white/[.1] rounded-full px-4 py-2.5" placeholder="Ton email" />
-            <ActionButton>Envoyer</ActionButton>
-          </div>
+      <Navigation action="close" />
+      <div className="flex flex-col flex-grow  p-8 sm:p-4 gap-16">
+        <div className="flex flex-col gap-8 pt-16">
+          <HeroTitle title="Mot de passe oublié ?" />
+          <p>Saisis ton adresse e-mail. Tu recevras un lien pour créer un nouveau mot de passe.</p>
+        </div>
+        <div className="flex flex-col gap-6">
+          <input className="placeholder:text-white bg-white/[.1] rounded-full px-4 py-2.5" placeholder="Ton email" />
+          <ActionButton>Envoyer</ActionButton>
         </div>
       </div>
     </>

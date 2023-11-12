17823a6afebe74f05941e8cf0bf246f03a894e95
Lionel Gatibelza <lionelgatibelza@gmail.com>
2023-11-12 13:51:54 +0100

    fix background image

--- app/components/BackgroundImage.tsx
+++ app/components/BackgroundImage.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 export default function BackgoundImage({ src }: IProps) {
   return (
-    <div>
+    <div className="absolute top-0 bottom-0 w-full z-[-12]">
       <div className="w-full h-full absolute top-0 left-0 z-[-10]"
         style={{
           background: `linear-gradient(to bottom, rgba(36, 42, 56, 0.3), rgba(36, 42, 56, 1) 80%)`
